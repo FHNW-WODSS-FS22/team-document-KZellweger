24c1342fc60ccc18f106c5af010fb2be3e299e41
Joshua Villing <joshua.villing@mimacom.com>
2022-04-05 07:42:22 +0200

    Keep state of document in backend and apply updateCommand there

--- backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/domain/document/Document.kt
+++ backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/domain/document/Document.kt
@@ -2,7 +2,10 @@ package ch.fhnw.woweb.teamdocumentserver.domain.document
 
 class Document(
     val paragraphs: List<Paragraph> = mutableListOf()
-)
+) {
+
+
+}
 
 
 
--- backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/service/DocumentProcessor.kt
+++ backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/service/DocumentProcessor.kt
@@ -1,28 +1,44 @@
 package ch.fhnw.woweb.teamdocumentserver.service
 
+import ch.fhnw.woweb.teamdocumentserver.domain.command.CommandType.*
 import ch.fhnw.woweb.teamdocumentserver.domain.command.DocumentCommand
 import ch.fhnw.woweb.teamdocumentserver.domain.document.Document
+import ch.fhnw.woweb.teamdocumentserver.domain.document.Paragraph
+import ch.fhnw.woweb.teamdocumentserver.service.TestDataUtil.createPlaceholderDocument
+import com.google.gson.Gson
 import org.springframework.stereotype.Service
 
 @Service
 class DocumentProcessor(
     // TODO: Add repository
-    val document: Document = Document()
+    val document: Document = createPlaceholderDocument()
 ) {
 
+    fun getFullDocument(): Document {
+        return document
+    }
+
     // TODO: Receive command instead of string
-    fun process(cmd: DocumentCommand): DocumentCommand {
+    fun process(cmd: DocumentCommand): DocumentCommand =
 
-//        when(cmd.type) {
-//            GENERIC -> TODO()
-//            INITIAL -> TODO()
-//            ADD_PARAGRAPH -> TODO()
-//            UPDATE_PARAGRAPH -> TODO()
-//            UPDATE_PARAGRAPH_ORDINALS -> TODO()
-//            UPDATE_AUTHOR -> TODO()
-//        }
+        when(cmd.type) {
+            INITIAL -> TODO()
+            ADD_PARAGRAPH -> TODO()
+            UPDATE_PARAGRAPH -> updateParagraph(cmd)
+            UPDATE_PARAGRAPH_ORDINALS -> TODO()
+            UPDATE_AUTHOR -> TODO()
+            GENERIC -> TODO()
+        }
 
+    fun updateParagraph(cmd: DocumentCommand): DocumentCommand {
+        val p = Gson().fromJson(cmd.payload, Paragraph::class.java)
+        document
+            .paragraphs
+            .find { it.id == p.id }
+            ?.content = p.content
         return cmd;
     }
+
+
 
 }
