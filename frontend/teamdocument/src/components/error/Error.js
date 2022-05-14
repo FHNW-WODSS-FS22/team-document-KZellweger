84d6601dd9e45fb6afd16291f67d23454d283309
Joshua Villing <joshua.villing@mimacom.com>
2022-05-14 11:47:41 +0200

    Add gray overlay while error is present

--- frontend/teamdocument/src/components/error/Error.js
+++ frontend/teamdocument/src/components/error/Error.js
@@ -5,14 +5,14 @@ import {useSelector} from "react-redux";
 const Error = () => {
     const error = useSelector(state => state.error);
 
-    return <div>
-            <div className={"modal " + (error.isPresent ? "show-modal" : "") }>
-                <div className="modal-content">
-                    <h1>Error</h1>
-                    <h3>{error.message}</h3>
-                </div>
+    return <div className={(error.isPresent ? "overlay" : "")}>
+        <div className={"modal " + (error.isPresent ? "show-modal" : "")}>
+            <div className="modal-content">
+                <h1>Error</h1>
+                <h3>{error.message}</h3>
             </div>
         </div>
+    </div>
 }
 
 export default Error;
