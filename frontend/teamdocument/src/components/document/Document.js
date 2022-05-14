3a91622ddc2bd1332ac75211269a19618fd62dbf
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-05-14 18:59:32 +0200

    ignore timeout error, since tests evidently shows that eventsource still is working after that

--- frontend/teamdocument/src/components/document/Document.js
+++ frontend/teamdocument/src/components/document/Document.js
@@ -14,8 +14,13 @@ const Document = () => {
     const esRef = useRef(null);
 
     useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('localUser'))
         if (!esRef.current) {
-            const eventSource = new EventSource(process.env.REACT_APP_BACKEND_BASE + '/document', {withCredentials:true});
+            const eventSource = new EventSourcePolyfill(process.env.REACT_APP_BACKEND_BASE + '/document',{
+                headers: {
+                    'Authorization': 'Basic ' + user.authdata
+                }
+            });
             eventSource.onmessage = msg => {
                 const cmd = JSON.parse(msg.data)
                 if (cmd.sender !== author.id) {
@@ -24,35 +29,14 @@ const Document = () => {
             }
             eventSource.onerror = err => {
                 console.log(err)
-                dispatch({type: 'ERROR', value: err})
+                if(err.error && err.error.message && err.error.message.includes("No activity within 45000")){
+                    console.log("Ignore this one since it is not relevant", err)
+                } else {
+                    dispatch({type: 'ERROR', payload: { isPresent: true, message: "Server is not available." }})
+                }
             }
             esRef.current = eventSource;
             return () => esRef.current.close()
-        }
-    }, []);
-
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('localUser'))
-        if (user && user.authdata) {
-            if (!esRef.current) {
-                const eventSource = new EventSourcePolyfill(process.env.REACT_APP_BACKEND_BASE + '/document', {
-                    headers: {
-                        'Authorization': 'Basic ' + user.authdata
-                    }
-                });
-                eventSource.onmessage = msg => {
-                    const cmd = JSON.parse(msg.data)
-                    if (cmd.sender !== author.id) {
-                        dispatch({type: cmd.type, payload: JSON.parse(cmd.payload)})
-                    }
-                }
-                eventSource.onerror = err => {
-                    console.log(err)
-                    dispatch({type: 'ERROR', value: err})
-                }
-                esRef.current = eventSource;
-                return () => esRef.current.close()
-            }
         }
     }, []);
 
