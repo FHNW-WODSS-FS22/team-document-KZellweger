df80f73b876a8c7be63cdad3bb48d1bcdb8349ad
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-05-15 11:30:17 +0200

    add debounce hook template

---
+++ frontend/teamdocument/src/hooks/debounce.hook.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+
+const useDebounce = (delay, inputValue, defaultValue) => {
+    const [value, setValue] = useState(defaultValue);
+    useEffect(
+        () => {
+            const handler = setTimeout(() => {
+                setValue(inputValue);
+            }, delay);
+            return () => { // Cleanup on input change
+                clearTimeout(handler);
+            };
+        },
+        [inputValue] // Execute on every input change
+    );
+
+    return value;
+};
+
+export default useDebounce;
