a38f1cc35967fb12e08bc32c5c5b40b8fe6e6cb0
Kevin Zellweger <kevin.zellweger@students.fhnw.ch>
2022-05-17 00:04:30 +0200

    use images and some small styles

--- frontend/teamdocument/src/components/navbar/Navbar.js
+++ frontend/teamdocument/src/components/navbar/Navbar.js
@@ -5,20 +5,10 @@ import {useSelector} from "react-redux";
 import Paragraph from "../document/paragraph/Paragraph";
 
 const Navbar = () => {
-
-    const otherAuthors = useSelector(state => state.otherAuthors);
-
-
-
     return (
         <div className="navbar dark-primary-color">
             <h1 className="text-primary-color">Google Docs Light</h1>
             <Logout />
-            <h1> Authors </h1>
-            {
-                otherAuthors
-                    .map(a => <p className="text-primary-color" key={a.id} id={a.id}>{a.name}</p>)
-            }
         </div>
     )
 }
