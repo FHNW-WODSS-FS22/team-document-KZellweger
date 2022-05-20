f5007a26c8d5b6e5fa5dbce400cccbf24bd524c9
Joshua Villing <joshua.villing@mimacom.com>
2022-05-20 07:42:23 +0200

    Remove not needed parameter

--- backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/service/DocumentProcessor.kt
+++ backend/TeamDocumentServer/src/main/kotlin/ch/fhnw/woweb/teamdocumentserver/service/DocumentProcessor.kt
@@ -98,14 +98,14 @@ class DocumentProcessor(
 
     private fun updateParagraphOrdinals(cmd: DocumentCommand): Flux<DocumentCommand> = lock.withLock {
         val paragraphs = Gson().fromJson(cmd.payload, Array<Paragraph>::class.java)
-        paragraphs.forEach { updateParagraphOrdinals(cmd, it) }
+        paragraphs.forEach { updateParagraphOrdinals(it) }
         return Flux.merge(just(cmd), resolveOrdinalsConflicts())
     }
 
-    private fun updateParagraphOrdinals(cmd: DocumentCommand, paragraph: Paragraph) {
+    private fun updateParagraphOrdinals(paragraphFromCmd: Paragraph) {
         document.paragraphs
-            .find { it.id == paragraph.id }
-            ?.ordinal = paragraph.ordinal
+            .find { it.id == paragraphFromCmd.id }
+            ?.ordinal = paragraphFromCmd.ordinal
     }
 
     private fun updateLock(cmd: DocumentCommand): Flux<DocumentCommand> = lock.withLock {
